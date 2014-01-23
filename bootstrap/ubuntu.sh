4699cfe44c6b88885a7fd7f197ce73eb4370887d
x3enos <x3enos@gmail.com>
2014-01-23 16:38:45 +0200

    [^] Change default ruby version in ubuntu.sh

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -3,6 +3,6 @@
 if type -p chef-solo > /dev/null; then
   echo "Using chef-solo at `which chef-solo`"
 else
-  apt-get install -y ruby ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert curl rubygems
+  apt-get install -y ruby1.9.3 ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert curl rubygems
   gem install chef --version='11.4.2' --no-ri --no-rdoc
 fi
